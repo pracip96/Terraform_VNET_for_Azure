122caa5811acf530054b630e2f808b5fa149ff81
pracip96 <92011066+pracip96@users.noreply.github.com>
2023-05-04 07:26:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  subscription_id = "cafa5f5c-f197-4058-897b-3e26f8ae3fa7"
-  client_id = "dedc9c87-4e2f-4c2e-a337-a2f1a7390643"
-  client_secret = "zaE8Q~2rzrMHEixn9YG9TIHlmC4Izc8RCi9GMcvm"
-  tenant_id = "a1ebca50-b870-423e-971e-98dbdbe96f11"
+  subscription_id = ""
+  client_id = ""
+  client_secret = ""
+  tenant_id = ""
   features {}
 
   }
